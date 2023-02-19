67688edab6c7630da002351c33a7b8e5c1ba6208
BaraqFi <Mbarrack01@gmail.com>
2023-02-19 10:02:49 -0500

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
--- prints full description of the table first_table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+-- Prints full desription of the table.
+SHOW CREATE TABLE `first_table`;
