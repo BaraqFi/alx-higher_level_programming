6f3bc8a600779f2fe31b3e1c8fa3a4bf88298e16
BaraqFi <mbarrack01@gmail.com>
2023-02-19 06:51:47 -0800

    intro done

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- lists all records od second_table in DB with conditions attached to the listing
+SELECT `score`, `name` FROM `second_table`
+ORDER BY `score` DESC, `name` DESC;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- displays no of records of id=89 in DB table first_table
+SELECT COUNT(*) FROM hbtn_0c_0.first_table WHERE id = 89;
