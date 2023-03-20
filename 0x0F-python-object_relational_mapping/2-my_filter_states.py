2c2d370e6f8a96d9adbacec5f00175475f74f1c2
BaraqFi <mbarrack01@gmail.com>
2023-03-20 04:52:52 +0000

    2-fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
-
-""""
- A script that lists all states from the database hbtn_0e_0_usa
-    starting with capital letter N
-    Username, password and database names are given as user args
-"""
-
+"""script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
 
 import sys
 import MySQLdb
 
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_pwd = sys.argv[2]
+    db_name = sys.argv[3]
+    search_name = sys.argv[4]
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         host='localhost',
-                         port=3306)
+    db = MySQLdb.connect(user=mysql_user,
+                         passwd=mysql_pwd,
+                         db=db_name,
+                         port=3306,
+                         host='localhost')
 
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states\
-                    WHERE name LIKE BINARY 'N%'\
-                    ORDER BY id ASC")
-
-    data = cursor.fetchall()
-
-    for row in data:
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id"
+                .format(search_name))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
 
-    cursor.close()
+    cur.close()
     db.close()
